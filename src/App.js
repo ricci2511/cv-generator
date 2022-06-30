7467d157c471f7246fb42a4d5a8b5a774591dc61
ricci2511 <ricci2511@protonmail.com>
2022-07-01 00:42:39 +0200

    App component rewritten to a functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
-import { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import { CssBaseline } from '@mui/material';
 import Footer from './components/Footer';
 import { Container } from '@mui/system';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return(
+const App = () => {
+    return (
         <>
-          <CssBaseline></CssBaseline>
-          <Header />
-          <Container component="main" maxWidth="xl">
-            <Main />
-          </Container>
-          <Footer />
+            <CssBaseline></CssBaseline>
+            <Header />
+            <Container component="main" maxWidth="xl">
+                <Main />
+            </Container>
+            <Footer />
         </>
     );
-  }
-}
+};
 
 export default App;
