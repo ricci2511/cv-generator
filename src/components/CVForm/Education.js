d4fcce553c981e60a1b36516d013318738badba7
ricci2511 <ricci2511@protonmail.com>
2022-06-30 17:29:39 +0200

    Both Eduaction components rewritten to functional components

--- src/components/CVForm/Education.js
+++ src/components/CVForm/Education.js
@@ -1,42 +1,46 @@
-import { Box } from "@mui/system";
-import { Component } from "react";
-import CVSection from "./CVSection";
-import { Button } from "@mui/material";
+import { Box } from '@mui/system';
+import CVSection from './CVSection';
+import { Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { getFormEducations } from "../Utils/getSections";
+import { getFormEducations } from '../Utils/getSections';
 
-class Education extends Component {
-    render() {
-        const { 
-            education, 
-            addEducation, 
-            deleteEducation, 
-            changeEducation, 
-            changeDate 
-        } = this.props;
+import React from 'react';
 
-        const educationSections = getFormEducations(education, deleteEducation, changeEducation, changeDate);
+const Education = ({
+    education,
+    addEducation,
+    deleteEducation,
+    changeEducation,
+    changeDate,
+}) => {
+    const educationSections = getFormEducations(
+        education,
+        deleteEducation,
+        changeEducation,
+        changeDate
+    );
 
-        return(
-            <CVSection title="Education">
-                <Box sx={{ 
+    return (
+        <CVSection title="Education">
+            <Box
+                sx={{
                     display: 'flex',
                     flexDirection: 'column',
                     gap: 2,
-                 }}>
-                    {educationSections}
-                    <Button 
-                        variant="contained" 
-                        fullWidth 
-                        startIcon={<AddIcon />}
-                        onClick={addEducation}
-                    >
-                        Add
-                    </Button>
-                </Box>
-            </CVSection>
-        );
-    }
-}
+                }}
+            >
+                {educationSections}
+                <Button
+                    variant="contained"
+                    fullWidth
+                    startIcon={<AddIcon />}
+                    onClick={addEducation}
+                >
+                    Add
+                </Button>
+            </Box>
+        </CVSection>
+    );
+};
 
 export default Education;
