eebf4b659d547825d1f43d370e472d8a52845583
ricci2511 <ricci2511@protonmail.com>
2022-07-01 00:18:11 +0200

    Refactored back to class component because of react-to-print

--- src/components/CVPreview/CVPreview.js
+++ src/components/CVPreview/CVPreview.js
@@ -1,29 +1,35 @@
 import { Box } from '@mui/material';
 import Header from './Header';
 import MainContent from './MainContent';
-import React from 'react';
+import React, { Component } from 'react';
 
-const CVPreview = ({ generalInformation, education, experience, skills }) => {
-    return (
-        <Box
-            component="article"
-            sx={{
-                position: 'sticky',
-                top: 10,
-                mb: 2,
-                backgroundColor: 'white',
-                minHeight: '297mm',
-            }}
-        >
-            <Header generalInformation={generalInformation} />
-            <MainContent
-                generalInformation={generalInformation}
-                education={education}
-                experience={experience}
-                skills={skills}
-            />
-        </Box>
-    );
-};
+// react-to-print requires the component to be printed to be a class component
+class CVPreview extends Component {
+    render() {
+        const { generalInformation, education, experience, skills } =
+            this.props;
+
+        return (
+            <Box
+                component="article"
+                sx={{
+                    position: 'sticky',
+                    top: 10,
+                    mb: 2,
+                    backgroundColor: 'white',
+                    minHeight: '297mm',
+                }}
+            >
+                <Header generalInformation={generalInformation} />
+                <MainContent
+                    generalInformation={generalInformation}
+                    education={education}
+                    experience={experience}
+                    skills={skills}
+                />
+            </Box>
+        );
+    }
+}
 
 export default CVPreview;
