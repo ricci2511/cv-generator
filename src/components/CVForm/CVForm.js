9d1309ffdaa504b842a4501f3c14af7429c216f3
ricci2511 <ricci2511@protonmail.com>
2022-06-30 17:34:48 +0200

    CVForm and CVSection components rewritten to functional components

--- src/components/CVForm/CVForm.js
+++ src/components/CVForm/CVForm.js
@@ -1,63 +1,60 @@
-import { Box } from "@mui/material";
-import { Component } from "react";
-import Education from "./Education";
-import Experience from "./Experience";
-import GeneralInformation from "./GeneralInformation";
-import Skills from "./Skills";
+import { Box } from '@mui/material';
+import Education from './Education';
+import Experience from './Experience';
+import GeneralInformation from './GeneralInformation';
+import Skills from './Skills';
 
-class CVForm extends Component {
-    render() {
-        const { 
-            generalInformation,
-            changeGeneralInformation,
-            education,
-            addEducation,
-            deleteEducation,
-            changeEducation,
-            experience,
-            addExperience,
-            deleteExperience,
-            changeExperience,
-            changeDate,
-            skills,
-            addSkill,
-            deleteSkill,
-         } = this.props;
+import React from 'react';
 
-        return(
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: 1
-                }}
-            >
-                <GeneralInformation 
-                    generalInformation={generalInformation} 
-                    changeGeneralInformation={changeGeneralInformation} 
-                />
-                <Education 
-                    education={education}
-                    addEducation={addEducation}
-                    deleteEducation={deleteEducation}
-                    changeEducation={changeEducation}
-                    changeDate={changeDate}
-                />
-                <Experience 
-                    experience={experience}
-                    addExperience={addExperience}
-                    deleteExperience={deleteExperience}
-                    changeExperience={changeExperience}
-                    changeDate={changeDate}
-                />
-                <Skills 
-                    skills={skills}
-                    addSkill={addSkill}
-                    deleteSkill={deleteSkill}
-                />
-            </Box>
-        );
-    }
-}
+const CVForm = ({
+    generalInformation,
+    changeGeneralInformation,
+    education,
+    addEducation,
+    deleteEducation,
+    changeEducation,
+    experience,
+    addExperience,
+    deleteExperience,
+    changeExperience,
+    changeDate,
+    skills,
+    addSkill,
+    deleteSkill,
+}) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 1,
+            }}
+        >
+            <GeneralInformation
+                generalInformation={generalInformation}
+                changeGeneralInformation={changeGeneralInformation}
+            />
+            <Education
+                education={education}
+                addEducation={addEducation}
+                deleteEducation={deleteEducation}
+                changeEducation={changeEducation}
+                changeDate={changeDate}
+            />
+            <Experience
+                experience={experience}
+                addExperience={addExperience}
+                deleteExperience={deleteExperience}
+                changeExperience={changeExperience}
+                changeDate={changeDate}
+            />
+            <Skills
+                skills={skills}
+                addSkill={addSkill}
+                deleteSkill={deleteSkill}
+            />
+        </Box>
+    );
+};
 
 export default CVForm;
